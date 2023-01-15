2e6ab5c97a2717a21caa236f3cfdeade2772d55c
Vladimir Zorov <vl.zorov@gmail.com>
2023-01-15 19:31:11 +0200

    add

--- src/restaurant/Main.java
+++ src/restaurant/Main.java
@@ -13,6 +13,13 @@ public class Main {
 
 
         Salmon salmon = new Salmon("taranka", priceNew);
+        Cake cake = new Cake(newName);
+        Coffee coffee = new Coffee("moka",priceNew, 85, 30);
+        Tea tea = new Tea(newName, priceNew, 150);
+        Soup soup = new Soup("ribenaChorba", priceNew, 300);
+
+
+        System.out.println("Total price is:" + salmon.getName());
 
     }
 }
